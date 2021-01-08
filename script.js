28a5003d9cb1859e76e99fd629d98697dc91a9ce
Amad Salmon <amad.salmon@icloud.com>
2021-01-08 22:26:01 +0100

    make site title unselectable

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+
+
+/*
+document.getElementById("toggleButton").addEventListener("click", myFunction, true);
+
+var toggleButton = document.querySelector(".topnav-toggle_button");
+var topNavItems = document.querySelector(".topnav-items");
+
+console.log("hello");
+if (toggleButton != null) {
+	console.log("toggleButton != null");
+	toggleButton.addEventListener('click', toggleActiveClass);
+} else {
+	console.log("toggleButton == null");
+	toggleButton = document.querySelector("#toggleButton");
+	if (toggleButton != null) {
+	console.log("toggleButton no longer null");
+	toggleButton.addEventListener('click', toggleActiveClass);
+	}
+}
+
+
+function toggleActiveClass() {
+	topNavItems.classList.toggle("active");
+	console.log("toggleButton click\n");
+}
+*/
 function toggleActive(x) {
 	x.classList.toggle("active");
 }
