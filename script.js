3b83e54edefb5842f479fdfe3e789711cd0e7ec3
Amad Salmon <amad.salmon@icloud.com>
2021-01-09 21:57:23 +0100

    refactor: better use of JavaScript

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-
-
-/*
-document.getElementById("toggleButton").addEventListener("click", myFunction, true);
-
-var toggleButton = document.querySelector(".topnav-toggle_button");
+var toggleButton = document.getElementById("toggleMenuButton");
 var topNavItems = document.querySelector(".topnav-items");
 
-console.log("hello");
-if (toggleButton != null) {
-	console.log("toggleButton != null");
-	toggleButton.addEventListener('click', toggleActiveClass);
-} else {
-	console.log("toggleButton == null");
-	toggleButton = document.querySelector("#toggleButton");
-	if (toggleButton != null) {
-	console.log("toggleButton no longer null");
-	toggleButton.addEventListener('click', toggleActiveClass);
-	}
+toggleButton.addEventListener("click", toggleMenuButtonClicked);
+
+function toggleActiveClassOntoggleMenuButton() {
+	toggleButton.classList.toggle("active");
 }
 
-
-function toggleActiveClass() {
-	topNavItems.classList.toggle("active");
-	console.log("toggleButton click\n");
-}
-*/
-function toggleActive(x) {
-	x.classList.toggle("active");
+function toggleMenuButtonClicked(){
+	toggleActiveClassOntoggleMenuButton();
 }
