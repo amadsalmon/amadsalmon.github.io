43f3b83f024eaf77ee68ba218587ba309e54c661
Amad Salmon <amad.salmon@icloud.com>
2021-01-10 00:42:53 +0100

    Make topNavItems disappear when clicking on one of its links.

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ var topNavItems = document.querySelector(".topnav-items");
 
 toggleButton.addEventListener("click", toggleMenuButtonClicked);
 
+/**
+* Make topNavItems disappear when clicking on one of its links.
+*/
+document.querySelectorAll('.topnav-items a')
+	.forEach(item => { 
+	item.addEventListener('click', event => {
+		if (topNavItems.classList.contains("active")){
+			topNavItems.classList.remove("active");
+		}
+  })
+})
+
 function toggleActiveClassOntoggleMenuButton() {
 	toggleButton.classList.toggle("active");
 }
 
 function toggleMenuButtonClicked(){
 	toggleActiveClassOntoggleMenuButton();
-	topNavItems.classList.toggle("responsive");
+	topNavItems.classList.toggle("active");
 }
