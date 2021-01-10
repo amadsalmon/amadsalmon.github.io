90a6870b60e5d97cf3204a336a1914fffe791a13
Amad Salmon <amad.salmon@icloud.com>
2021-01-10 01:19:40 +0100

    Underline the active topnav item

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 var toggleButton = document.getElementById("toggleMenuButton");
 var topNavItems = document.querySelector(".topnav-items");
+var activeTopNavItem = document.querySelector(".topnav-items a.active");
 
 toggleButton.addEventListener("click", toggleMenuButtonClicked);
 
-/**
-* Make topNavItems disappear when clicking on one of its links.
-*/
-document.querySelectorAll('.topnav-items a')
-	.forEach(item => { 
-	item.addEventListener('click', event => {
-		if (topNavItems.classList.contains("active")){
-			topNavItems.classList.remove("active");
-		}
-  })
-})
+document.querySelectorAll('.topnav-items a').forEach(
+	item => { 
+		item.addEventListener('click', topNavItemLinkClicked)
+	})
 
 function toggleActiveClassOntoggleMenuButton() {
 	toggleButton.classList.toggle("active");
@@ -23,3 +17,17 @@ function toggleMenuButtonClicked(){
 	toggleActiveClassOntoggleMenuButton();
 	topNavItems.classList.toggle("active");
 }
+
+function topNavItemLinkClicked(){
+	/* Make topNavItems disappear when clicking on one of its links. */
+	if (topNavItems.classList.contains("active")){
+			topNavItems.classList.remove("active");
+	}
+	
+	/* Give underline to correct topnav item */
+	activeTopNavItem.classList.remove("active");
+	this.classList.add("active");
+	activeTopNavItem = this;
+	
+}
+
